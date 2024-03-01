f140a44abe9d060e614ddd328cb60b6582de6a40
Vibhanshu Pandey <pandeyvibhanshu007@gmail.com>
2024-03-01 23:04:50 +0530

    API key added

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -1,23 +1,29 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
 
 function newQuote() {
     // pick a random quote afrom apiQuotes
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 } 
-// // Get quotes from API
-// async function getQuotes() {
-//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error) {
-//         // catch error here
-//     }     
-// }
+// Get quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // catch error here
+    }     
+}
 
 // // on load
-// getQuotes();
-
-newQuote();
+getQuotes();
