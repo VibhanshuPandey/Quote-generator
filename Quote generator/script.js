a2500ff9d11a118bc0aef87a5d6e48aa467a1062
Vibhanshu Pandey <pandeyvibhanshu007@gmail.com>
2024-03-02 23:03:50 +0530

    updated

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -45,5 +45,9 @@ function tweetQuote() {
     const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank')
 }
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click')
 // // on load
 getQuotes();
